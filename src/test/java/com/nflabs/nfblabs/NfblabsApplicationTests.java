d81193850b3432165a774d64d01745c860c106fa
Bruno <brvno.v2@gmail.com>
2025-06-04 21:13:38 -0400

    nuevo proyecto

---
+++ src/test/java/com/nflabs/nfblabs/NfblabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nflabs.nfblabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfblabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
