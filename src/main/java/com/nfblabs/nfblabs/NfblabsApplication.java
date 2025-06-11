bdff7f55cf2260ba2cbc3623adce49d31ceb067c
brvno <brvno.v2@gmail.com>
2025-06-11 00:14:33 -0400

    nuevo proyecto

---
+++ src/main/java/com/nfblabs/nfblabs/NfblabsApplication.java
@@ -0,0 +1,13 @@
+package com.nfblabs.nfblabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfblabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfblabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nfblabs/nfblabs/NfblabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfblabs.nfblabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfblabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
